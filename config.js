c81173b5e362399335ee1cdf9a40a434565f154c
nopanmaker <117498594+nopanmaker@users.noreply.github.com>
2022-11-05 18:43:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -17,13 +17,13 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'THE DANIXD'
+global.ownername = 'THE Nopan'
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['6281251549899','6281251549899','6281251549899']
-global.premium = ['6281251549899']
+global.owner = ['6285862754807','6285862754807','6285862754807']
+global.premium = ['6285862754807']
 global.packname = 'The Jo Bot'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
